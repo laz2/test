84711d14c3d40ba5c2bd4f6c3284ebf9285147ce
Dmitry Lazurkin <dilaz03@gmail.com>
2014-02-23 00:37:29 +0300

    try generator-angular-fullstack

--- js/angular/angular-gettext/translations.js
+++ js/angular/angular-gettext/translations.js
@@ -1,4 +1,4 @@
 angular.module("gettext").run(['gettextCatalog', function (gettextCatalog) {
-    gettextCatalog.setStrings('ru', {"Gettext test":"тест","Input text":"Введите текст","No translation":"","Welcome {{name}}!":"Привет {{name}}!"});
+    gettextCatalog.setStrings('ru', {"Gettext test":"тест","Input text":"Введите текст","No translation":"","Welcome {{name}}!":"Привет {{name}}!","one task":"одна задача"});
 
 }]);
